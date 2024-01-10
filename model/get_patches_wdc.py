c1d0be3fcd1e94228065b47c5856c854bf8340f4
KikkiZ <zhangyozh@foxmail.com>
2024-01-10 21:08:29 +0800

    修改数据生成的代码

--- model/get_patches_wdc.py
+++ model/get_patches_wdc.py
@@ -1,69 +1,75 @@
-from glob import glob
-from tqdm import tqdm
-import numpy as np
-import os
-from natsort import natsorted
-from joblib import Parallel, delayed
-import multiprocessing
 import argparse
+import os
+from glob import glob
+
+import numpy as np
+from joblib import Parallel, delayed
+from natsort import natsorted
 from scipy.io import savemat, loadmat
+from tqdm import tqdm
 
-
+"""
+该段代码用于生成训练数据集
+"""
 parser = argparse.ArgumentParser(description='Generate patches from Full Resolution mat files')
 parser.add_argument('--src_dir', default='../datasets/WDC/', type=str, help='Directory for full resolution images')
 parser.add_argument('--tar_dir', default='../datasets/WDC/train', type=str, help='Directory for image patches')
-parser.add_argument('--ps', default=64, type=int, help='Image Patch Size')
-parser.add_argument('--num_patches', default=1024, type=int, help='Number of patches per image')
+parser.add_argument('--ps', default=64, type=int, help='Image Patch Size')  # 单个样本的大小 64x64
+parser.add_argument('--num_patches', default=1024, type=int, help='Number of patches per image')  # 样本数量
 parser.add_argument('--num_cores', default=8, type=int, help='Number of CPU Cores')
-parser.add_argument('--train_flag', action='store_true',default=True)
-parser.add_argument('--K', default=24, type=int, help='Number of bands')
+parser.add_argument('--train_flag', action='store_true', default=True)
+parser.add_argument('--K', default=24, type=int, help='Number of bands')  # 频段数
 
 args = parser.parse_args()
 
-src = args.src_dir
-tar = args.tar_dir
-PS = args.ps
 NUM_PATCHES = args.num_patches
 NUM_CORES = args.num_cores
+ADJ_BANDS = args.K
+
+src = args.src_dir
+tar = args.tar_dir
+patch_size = args.ps
 train_flag = args.train_flag
-K = args.K
 
-if os.path.exists(tar):
-    os.system("rmdir -r {}".format(tar))
+# 检查target路径是否存在, 不存在则创建路径
+if not os.path.exists(tar):
+    os.makedirs(tar)
 
-os.makedirs(tar)
-
-#get sorted folders
-files = natsorted(glob(os.path.join(src, '*.mat')))
-
+# natsorted用于对字符串进行自然排序, glob用于匹配特定模式的文件路径名
+files = natsorted(glob(src + '/*.mat'))
 print(files)
 
-def save_output(dir, data, name):
-    mdic = {"gt": data}
-    savemat(dir + "/" + name + ".mat", mdic)
 
-def save_files(i):
+# 存储文件
+def save_output(path, data, name):
+    data_dict = {"gt": data}
+    savemat(path + "/" + name + ".mat", data_dict)
+
+
+def save_files(i, key):
     clean_file = files[i]
     clean_img = loadmat(clean_file)
 
-    H = clean_img['data'].shape[0]
-    W = clean_img['data'].shape[1]
-    bands = clean_img['data'].shape[-1]
-    clean = np.concatenate([clean_img['data'][..., range(int(K / 2), 0, -1)],
-                            clean_img['data'],
-                            clean_img['data'][
-                                ..., range(bands - 1, bands - int(K / 2), -1)]], axis=-1)
+    height = clean_img[key].shape[1]
+    width = clean_img[key].shape[2]
+    bands = clean_img[key].shape[0]
+
+    # 沿波段拓展
+    clean = np.concatenate([clean_img[key][range(int(ADJ_BANDS / 2), 0, -1), ...],
+                            clean_img[key],
+                            clean_img[key][range(bands - 1, bands - int(ADJ_BANDS / 2), -1), ...]], axis=0)
+
     for j in range(NUM_PATCHES):
-        rr = np.random.randint(0, H - PS)
-        cc = np.random.randint(0, W - PS)
-        kk = np.random.randint(0, bands)
+        h = np.random.randint(0, height - patch_size)
+        w = np.random.randint(0, width - patch_size)
+        b = np.random.randint(0, bands)
 
-        clean_patch = clean[rr:rr + PS, cc:cc + PS, kk:kk + K]
+        clean_patch = clean[b:b + ADJ_BANDS, h:h + patch_size, w:w + patch_size]
 
-        save_output(tar, clean_patch, 'patch_{}_{}'.format(i+1,j+1))
-
-Parallel(n_jobs=NUM_CORES)(delayed(save_files)(i) for i in tqdm(range(0, 1)))
+        save_output(tar, clean_patch, 'patch_{}_{}'.format(i + 1, j + 1))
 
 
-
-
+# Parallel用于并行计算
+# tqdm用于在cmd中添加进度条
+# delayed函数用于将需要并行计算的函数和参数包装, 便于Parallel并行计算
+Parallel(n_jobs=NUM_CORES)(delayed(save_files)(i, 'image') for i in tqdm(range(0, 1)))
--- model/get_patches_wdc_test.py
+++ model/get_patches_wdc_test.py
@@ -1,15 +1,16 @@
-
-from glob import glob
-from tqdm import tqdm
-import numpy as np
-import os
-from natsort import natsorted
-from joblib import Parallel, delayed
-import multiprocessing
 import argparse
+import os
+from glob import glob
+
+import numpy as np
+from joblib import Parallel, delayed
+from natsort import natsorted
 from scipy.io import savemat, loadmat
+from tqdm import tqdm
 
-
+"""
+该段代码用于生成测试数据集
+"""
 parser = argparse.ArgumentParser(description='Generate patches from Full Resolution mat files')
 parser.add_argument('--src_dir', default='../datasets/WDC/test', type=str, help='Directory for full resolution images')
 parser.add_argument('--tar_dir', default='../datasets/WDC/test/blind', type=str, help='Directory for image patches')
@@ -18,49 +19,46 @@ parser.add_argument('--num_patches', default=1, type=int, help='Number of patche
 parser.add_argument('--num_cores', default=8, type=int, help='Number of CPU Cores')
 parser.add_argument('--K', default=24, type=int, help='Number of bands')
 
-
 args = parser.parse_args()
+
+NUM_PATCHES = args.num_patches
+NUM_CORES = args.num_cores
+ADJ_BANDS = args.K
 
 src = args.src_dir
 tar = args.tar_dir
 PS = args.ps
-NUM_PATCHES = args.num_patches
-NUM_CORES = args.num_cores
-K=args.K
 
-if os.path.exists(tar):
-    os.removedirs(tar)
-os.makedirs(tar)
+if not os.path.exists(tar):
+    os.makedirs(tar)
 
-#get sorted folders
 files = natsorted(glob(os.path.join(src, '*.mat')))
-
 print(files)
 
 clean_file = files[0]
 print(clean_file)
 clean_img = loadmat(clean_file)
-bands = clean_img['gt'].shape[-1]
-clean = np.concatenate([clean_img['gt'][..., range(int(K / 2), 0, -1)],
-                                        clean_img['gt'],
-                                        clean_img['gt'][
-                                        ..., range(bands - 1, bands - int(K/2),  -1)]], axis=-1)
-noisy = np.concatenate([clean_img['input'][..., range(int(K / 2), 0, -1)],
-                                        clean_img['input'],
-                                        clean_img['input'][
-                                        ..., range(bands - 1, bands - int(K/2),  -1)]], axis=-1)
-def save_output(dir, clean, noisy, name):
-    mdic = {"gt": clean, "input": noisy}
-    savemat(dir + "/" + name + ".mat", mdic)
+bands = clean_img['gt'].shape[0]
+clean = np.concatenate([clean_img['gt'][range(int(ADJ_BANDS / 2), 0, -1), ...],
+                        clean_img['gt'],
+                        clean_img['gt'][range(bands - 1, bands - int(ADJ_BANDS / 2), -1), ...]], axis=0)
+noisy = np.concatenate([clean_img['noisy'][range(int(ADJ_BANDS / 2), 0, -1), ...],
+                        clean_img['noisy'],
+                        clean_img['noisy'][range(bands - 1, bands - int(ADJ_BANDS / 2), -1), ...]], axis=0)
+
+
+def save_output(path, gt, decrease, name):
+    data_dict = {"gt": gt, "input": decrease}
+    savemat(path + "/" + name + ".mat", data_dict)
+
 
 def save_files(i):
-    H = clean_img['gt'].shape[0]
-    W = clean_img['gt'].shape[1]
     for j in range(NUM_PATCHES):
         for k in range(0, bands, 1):
-            clean_patch = clean[i * PS:i * PS + PS, j * PS:j * PS + PS, k:k + int(K)]
-            noisy_patch = noisy[i * PS:i * PS + PS, j * PS:j * PS + PS, k:k + int(K)]
+            clean_patch = clean[k:k + int(ADJ_BANDS), i * PS:i * PS + PS, j * PS:j * PS + PS]
+            noisy_patch = noisy[k:k + int(ADJ_BANDS), i * PS:i * PS + PS, j * PS:j * PS + PS]
 
             save_output(tar, clean_patch, noisy_patch, 'patch_{}_{}_{}'.format(i + 1, j + 1, k + 1))
+
 
 Parallel(n_jobs=NUM_CORES)(delayed(save_files)(i) for i in tqdm(range(NUM_PATCHES)))
