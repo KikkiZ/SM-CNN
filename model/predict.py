d2e7598dbb488a617b0d0b080ffcea880a532467
KikkiZ <zhangyozh@foxmail.com>
2024-01-11 15:10:53 +0800

    调整模型测试代码

--- model/predict.py
+++ model/predict.py
@@ -14,19 +14,17 @@ from tqdm import tqdm
 from dataset_denoising import get_test_data
 from losses import CharbonnierLoss, L1Loss, MSELoss, SmoothL1Loss, L1MSELoss, MSETVLoss, L1TVLoss, SSLoss
 
-######### Set GPUs ###########
+# Set GPUs
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config-file',help='path to the config file')
+parser.add_argument('--config-file', help='path to the config file')
 args = parser.parse_args()
 
 with open(args.config_file) as fp:
     config = yaml.safe_load(fp)
     config = AttrDict(config)
-
-
 
 checkpoint_path = config.checkpoint_path
 num_epochs = config.epochs
@@ -43,14 +41,12 @@ save_dir = config.save_dir
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
 
-
-
 # Get cpu or gpu device for training.
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print("Using {} device".format(device))
 print('===> Loading datasets')
-img_options_train = {'patch_size':patch_size[0], 'K':K}
+img_options_train = {'patch_size': patch_size[0], 'K': K}
 
 test_data = get_test_data(test_file, img_options_train)
 
@@ -59,18 +55,17 @@ test_dataloader = DataLoader(test_data, batch_size=1, shuffle=False, drop_last=F
 len_trainset = test_dataloader.__len__()
 print("Sizeof training set: ", len_trainset)
 
-if arch == 'SM-CNN':
-    model = SMCNN(config.num_conv2d_filters, config.num_conv3d_filters, config.num_convolutionblock_filters,
-                    config.K)
-    model.apply(init_weight)
-else:
-    print('No model!')
+model = SMCNN(config.num_conv2d_filters,
+              config.num_conv3d_filters,
+              config.num_convolutionblock_filters,
+              config.K)
+model.apply(init_weight)
 
-#send model to device
+# send model to device
 model.to(device)
 
-#loss_fn = _loss
-#loss_fn = _loss
+# loss_fn = _loss
+# loss_fn = _loss
 if loss_type == 'L1':
     loss_fn = L1Loss().to(device)
 elif loss_type == 'MSE':
@@ -88,7 +83,8 @@ elif loss_type == 'L1TV':
 elif loss_type == 'SS':
     loss_fn = SSLoss(alpha=0.9).to(device)
 else:
-    assert 'Unexpected Loss!'
+    assert False, 'Unexpected Loss!'
+
 
 def test(dataloader, model, loss_fn):
     hsi_image_list = []
@@ -105,6 +101,10 @@ def test(dataloader, model, loss_fn):
             # send gpu
             clean_band, spectral_volume = clean_band.to(device), spectral_volume.to(device)
             spatial_image = spatial_image.to(device)
+
+            clean_band = torch.unsqueeze(clean_band, dim=0)
+            spatial_image = torch.unsqueeze(spatial_image, dim=0)
+            spectral_volume = torch.unsqueeze(spectral_volume, dim=0)
 
             # Compute prediction error
             dn_output = model(spatial_image, spectral_volume)
@@ -129,7 +129,7 @@ def test(dataloader, model, loss_fn):
 
 if os.path.isfile(checkpoint_path):
     checkpoint = torch.load(checkpoint_path)
-    #model.load_state_dict(checkpoint['model_state_dict'])
+    # model.load_state_dict(checkpoint['model_state_dict'])
     load_checkpoint(model, checkpoint)
     init_epoch = checkpoint['epoch']
     best_loss = checkpoint['loss']
@@ -144,7 +144,7 @@ test_loss_avg, clean, noisy, test_output, mpsnr, mssim = test(test_dataloader, m
 elapsed = time.time() - inittime
 print(f'Test completed... Elapsed = {elapsed:>.4f}')
 
-clean_image, noise_image, hsi_image = merged_patch(clean, noisy, test_output, shape=(256,256,191), patch=256)
+clean_image, noise_image, hsi_image = merged_patch(clean, noisy, test_output, shape=(200, 200, 191), patch=256)
 img_ar_noisy = np.clip(noise_image, 0, 1)
 img_ar_hsid = np.clip(hsi_image, 0, 1)
 img_ar_clean = np.clip(clean_image, 0, 1)
@@ -156,11 +156,11 @@ fig.subplots_adjust(wspace=0, hspace=0)
 
 axe11.set_title('Temiz görüntü', fontsize=12)
 axe11.imshow(img_ar_clean[:, :, [57, 27, 17]], cmap='gray')
-axe12.set_title('Gürültülü görüntü ($\sigma$=%d)' %(round(noise_level*255)), fontsize=12)
+axe12.set_title('Gürültülü görüntü ($\sigma$=%d)' % (round(noise_level * 255)), fontsize=12)
 axe12.imshow(img_ar_noisy[:, :, [57, 27, 17]], cmap='gray')
 axe13.set_title(arch + '(PSNR=%.3f, SSIM=%.3f)' % (mpsnr, mssim), fontsize=12)
 axe13.imshow(img_ar_hsid[:, :, [57, 27, 17]], cmap='gray')
 plt.tight_layout()
 plt.savefig(save_dir + '/rgb_' + train_type + ".png")
-#plt.show()
+# plt.show()
 print("Done!")
